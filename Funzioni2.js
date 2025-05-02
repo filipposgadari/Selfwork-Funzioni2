debd2131da770681d3bdf0aa10050d466816b585
filipposgadari <filipposgadari97@gmail.com>
2025-05-02 14:05:39 +0200

    Aggiornato in maniera corretta

--- Funzioni2.js
+++ Funzioni2.js
@@ -1,21 +1,20 @@
 function stampaNumeri(N) {
     
     for (let i = 1; i <= N; i++){
-        let conteggio;
         
-        if (i % 15 === 0) {
+        if (i % 15 == 0) {
             console.log('fizzBuzz');
-        } else if(i % 3 === 0){
+        } else if(i % 3 == 0){
             console.log('Fizz');
-        } else if(i % 5 === 0){
+        } else if(i % 5 == 0){
             console.log('Buzz');
         } else {
-            conteggio = i;
+            console.log(i);
+            
         }
-        console.log(conteggio);
     } 
 }
-stampaNumeri(20);
+stampaNumeri(25);
         
         
         
